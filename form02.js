341650f7d0de7b892925f6b4d31150b4fbe0f9d3
eldercamposds <117109983+eldercamposds@users.noreply.github.com>
2022-11-11 11:19:29 -0300

    Criando arquivo css

--- form02.js
+++ form02.js
@@ -1,1 +1,14 @@
 console.log("Rodando o formulário dinâmico")
+function setorsel(){
+    let selselector = document.getElementById('selsetor')
+    let opcaoValor = selselector.options[selselector.selectedIndex].value
+    let form = document.getElementById('form')
+    console.log(opcaoValor)
+    if(opcaoValor == 'setor01'){
+        form.action = 'https://formsubmit.co/eldercamposdev@gmail.com'
+    }else if(opcaoValor == 'setor02'){
+        form.action = "https://formsubmit.co/zerelder@gmail.com"
+    }else{
+        form.action = ''
+    }
+}
